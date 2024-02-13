f0b9dda70e2f296b4508e15e3fe512534e8b257e
partysoonxd <vladbelom2005@gmail.com>
2024-02-13 14:07:31 +0700

    made sign in form functionality

--- client/app/ui/SignIn/SignInForm.jsx
+++ client/app/ui/SignIn/SignInForm.jsx
@@ -1,10 +1,32 @@
-export default function SignInForm() {
-    return (
-        <form>
-            <input type="email" placeholder="email"/>
-            <input type="password" placeholder="password"/>
+"use client"
 
-            <button type="submit">SIGN UP</button>
-        </form>
+import { googleAuth, signIn } from "@/app/http/userAPI"
+
+export default function SignInForm() {
+    const formHandler = async(e) => {
+        try {
+            const formData = new FormData(e.currentTarget)
+            const data = Object.fromEntries(formData)
+    
+            await signIn(data).then(data => {
+                console.log(data)
+                localStorage.setItem("token", data.jwt)
+            })
+        } catch (error) {
+            
+        }
+    }  
+
+    return (
+        <>
+            <form onSubmit={e => formHandler(e)}>
+                <input type="text" placeholder="email or username" name="identifier"/>
+                <input type="password" placeholder="password" name="password"/>
+
+                <button type="submit">SIGN UP</button>
+            </form>
+            <button onClick={e => googleAuth()}>continue with google</button>
+        </>
+        
     )
 }
