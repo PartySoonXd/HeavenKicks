31283c39581924ce7c5d6cf75e62f7f420e851b3
partysoonxd <vladbelom2005@gmail.com>
2024-02-14 17:57:35 +0700

    complete google auth

--- client/app/(pages)/(home)/page.jsx
+++ client/app/(pages)/(home)/page.jsx
@@ -2,36 +2,46 @@
 
 import { useUserContext } from "@/app/lib/UserContext";
 import { useSearchParams } from "next/navigation";
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { observer } from "mobx-react-lite";
 
 import navigate from "@/app/lib/navigate";
 import { $apiHost } from "@/app/http";
+import {deleteToken, setToken} from "@/app/lib/tokenHandler";
 
-export default function Home() {
+export default observer(function Home() {
   const {user} = useUserContext()
   const searchParams = useSearchParams()
+  const authCode = searchParams.get('code')
 
-  useLayoutEffect(() => {
-    const authCode = searchParams.get('code')
-
+  useEffect(() => {
     const authGoogleUser = async() => {
-        await $apiHost.post("/strapi-google-auth/user-profile", {code: authCode})
-        .then(({data}) => {
-          localStorage.setItem("token", data.data.token)
-          navigate('/')
-        })
-        .catch(error => {
-          console.log(error)
-        })
+      try {
+        const {data} = await $apiHost.post("/strapi-google-auth/user-profile", {code: authCode})
+
+        setToken(data.data.token)
+        user.setIsAuth(true)
+        user.setUser(data.data.user)
+
+        navigate('/')
+      } catch (error) {
+        console.log(error)
+      }
     }
+    
     if (authCode) {
       authGoogleUser()
     }
   }, [])
+  const logout = async () => {
+    await deleteToken()
+    user.setIsAuth(false)
+  }
   return (
     <main className="content">
-        <p>{user ? `hello, ${user.id}`: "Please authorize"}</p>
+        <p>{user.user.id ? `hello, ${user.user.id}`: "Please authorize"}</p>
         <h1>Home page</h1>
+        <button type="button" onClick={() => logout()}>Logout</button>
     </main>
   );
-}
+})
