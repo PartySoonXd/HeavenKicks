8005921e38410fbc2deccc3c9102a021ddd7d0b4
partysoonxd <vladbelom2005@gmail.com>
2024-02-14 11:27:11 +0700

    add user provider

--- client/app/(pages)/layout.jsx
+++ client/app/(pages)/layout.jsx
@@ -1,6 +1,7 @@
 import "@/app/styles/globals.scss";
 import Header from "../components/Header/Header";
 import Newsletter from "../components/Newsletter/Newsletter";
+import UserProvider from "../components/UserProvider/UserProvider";
 
 export const metadata = {
   title: "HeavenKicks",
@@ -11,11 +12,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="container">
-          <Header/>
-          {children}
-        </div>
-        <Newsletter/>
+        <UserProvider>
+          <div className="container">
+            <Header/>
+            {children}
+          </div>
+          <Newsletter/>
+        </UserProvider>
       </body>
     </html>
   );
