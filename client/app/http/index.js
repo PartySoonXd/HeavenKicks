77505a4eb81d215aae55d3017a35eca1b4c7eae6
partysoonxd <vladbelom2005@gmail.com>
2024-02-14 11:28:09 +0700

    change baseURL

--- client/app/http/index.js
+++ client/app/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $apiHost = axios.create({
-    baseURL: "http://localhost:1337"
+    baseURL: "http://127.0.0.1:1337"
 })
 
 const $authApiHost = axios.create({
-    baseURL: "http://localhost:1337"
+    baseURL: "http://127.0.0.1:1337"
 })
 
 export {
