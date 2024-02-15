847e3c8e5ac0494ac901d103a70a2412c1f9a9b0
partysoonxd <vladbelom2005@gmail.com>
2024-02-15 18:04:02 +0700

    start developing checkout

--- client/app/(pages)/cart/page.jsx
+++ client/app/(pages)/cart/page.jsx
@@ -1,7 +1,21 @@
-export default function Cart() {
+"use client"
+
+import { useUserContext } from "@/app/lib/UserContext"
+import { observer } from "mobx-react-lite"
+
+export default observer(function Cart() {
+    const {user} = useUserContext()
+    const checkoutHandler = async() => {
+        
+    }
+    console.log(user.user.cart)
     return (
         <main className="content">
             <h1 className="">Cart</h1>
+            <div>
+                {user.user.cart}
+            </div>
+            <button type="button" onClick={checkoutHandler}>CHECKOUT</button>
         </main>
     )
-}
+})
