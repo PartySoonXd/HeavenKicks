315930b644924d50103f4c0280cfd8adb72f8342
partysoonxd <vladbelom2005@gmail.com>
2024-02-16 17:08:34 +0700

    get cart for user

--- client/app/components/UserProvider/UserProvider.jsx
+++ client/app/components/UserProvider/UserProvider.jsx
@@ -6,35 +6,44 @@ import { UserContext } from "@/app/lib/UserContext";
 import UserStore from "@/app/store/userStore";
 import { getToken } from "@/app/lib/tokenHandler";
 import { $apiHost } from "@/app/http";
-import { observer } from "mobx-react-lite";
 
-export default observer(function UserProvider({children}) {
+export default function UserProvider({children}) {
     const userStore = new UserStore()
     const [isAuth, setIsAuth] = useState(false)
-
+    
     useEffect(() => {
-        const checkUser = async() => {
+        const getCart = async(id, token) => {
+            await $apiHost.get(`/api/carts/${id}?populate=cart_items`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(({data}) => {
+                userStore.setCart(data.data.attributes)
+            })
+        }
+        const authUser = async() => {
             const token = await getToken()
             if (token) {
                 try {
-                    const {data} = await $apiHost.get('/api/users/me?fields=id&fields=email&fields=username&populate=cart', {
+                    await $apiHost.get('/api/users/me?fields=id&fields=email&fields=username&populate=cart', {
                         headers: {
                             Authorization: `Bearer ${token.value}` 
                         }
-                    }) 
-                    userStore.setUser(data)
-                    userStore.setIsAuth(true)
-                    setIsAuth(true)
-                    console.log(data)
+                    }).then(({data}) => {
+                        userStore.setUser(data)
+                        userStore.setIsAuth(true)
+                        setIsAuth(true)
+                        getCart(data.cart.id, token.value)
+                    })
                 } catch (error) {
                     console.log(error)
                 }
             }
         }
-        checkUser()
+        authUser()
+        
     }, [isAuth])
 
-    
     return (
         <UserContext.Provider
             value={{user: userStore}}
@@ -42,4 +51,4 @@ export default observer(function UserProvider({children}) {
             {children}
         </UserContext.Provider>
     )
-})
+}
