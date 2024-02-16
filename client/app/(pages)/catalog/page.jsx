1883fd0bbb41b0d600c867df4130fc81d132cd95
partysoonxd <vladbelom2005@gmail.com>
2024-02-16 17:07:11 +0700

    add products component

--- client/app/(pages)/catalog/page.jsx
+++ client/app/(pages)/catalog/page.jsx
@@ -1,7 +1,10 @@
+import Products from "@/app/ui/Catalog/Products/Products";
+
 export default function Catalog() {
     return (
         <main className="content">
             <h1 className="">Catalog</h1>
+            <Products/>
         </main>
     )
 }
