5e90ac62494cabc6c92a30cc24f25768b54463e0
partysoonxd <vladbelom2005@gmail.com>
2024-02-17 16:30:40 +0700

    create component for fixing auth bug

---
+++ client/app/components/IndexLayout/IndexLayout.jsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { useEffect } from "react"
+
+import Header from "../Header/Header"
+import Newsletter from "../Newsletter/Newsletter"
+import { useUserContext } from "@/app/lib/UserContext"
+import { $apiHost } from "@/app/http"
+import { getToken } from "@/app/lib/tokenHandler"
+import { observer } from "mobx-react-lite"
+
+export default observer(function IndexLayout({children}) {
+    const {user} = useUserContext()
+    
+    useEffect(() => {
+        const getCart = async(id, token) => {
+            await $apiHost.get(`/api/carts/${id}?populate=cart_items`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(({data}) => {
+                user.setCart(data.data.attributes)
+            })
+        }
+        const authUser = async() => {
+            const token = await getToken()
+            if (token) {
+                try {
+                    await $apiHost.get('/api/users/me?fields=id&fields=email&fields=username&populate=cart', {
+                        headers: {
+                            Authorization: `Bearer ${token.value}` 
+                        }
+                    }).then(({data}) => {
+                        user.setUser(data)
+                        user.setIsAuth(true)
+                        getCart(data.cart.id, token.value)
+                    })
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        }
+        authUser()  
+    }, [user.isAuth])
+    return (
+        <>
+        <div className="container">
+            <Header/>
+            {children}
+        </div>
+        <Newsletter/>
+        </>
+    )
+})
