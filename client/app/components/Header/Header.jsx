2a4549289a99791ba45461ab0472ad63d22a717b
partysoonxd <vladbelom2005@gmail.com>
2024-02-17 16:33:50 +0700

    add styles

--- client/app/components/Header/Header.jsx
+++ client/app/components/Header/Header.jsx
@@ -1,34 +1,37 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function Header() {
     return (
         <header className="header">
             <div className="logo">
-                <Link href="/">LOGO</Link>
+                <Link href="/">
+                    <Image src="Logo.svg" width={292} height={50} alt="HeavenKicks logo"/>                    
+                </Link>
             </div>
             <nav className="header-nav">
                 <ul className="header-nav-list">
                     <li className="header-nav-item">
-                        <Link href="/catalog">Catalog</Link>
+                        <Link href="/catalog" className="links">Catalog</Link>
                     </li>
                     <li className="header-nav-item">
-                        <Link href="/new-arrivals">New arrivals</Link>
+                        <Link href="/new-arrivals" className="links">New arrivals</Link>
                     </li>
                     <li className="header-nav-item">
-                        <Link href="/contacts">Contacts</Link>
+                        <Link href="/contacts" className="links">Contacts</Link>
                     </li>
                     <li className="header-nav-item">
-                        <Link href="/faq">FAQ</Link>
+                        <Link href="/faq" className="links">FAQ</Link>
                     </li>
                 </ul>
             </nav>
             <div className="separate-links">
-                <Link href="/sign-in">Sign in</Link>
-                <Link href="/sign-up">Sign up</Link>
+                <Link href="/sign-in" className="auth-button sign-in-btn links">Sign in</Link>
+                <Link href="/sign-up" className="auth-button sign-up-btn links">Sign up</Link>
             </div>
-            <div className="separate-links">
+            {/* <div className="separate-links">
                 <Link href="/cart">Cart</Link>
-            </div>
+            </div> */}
         </header>
     )
 }
