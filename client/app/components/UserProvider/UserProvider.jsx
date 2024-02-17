a2802ae39df7500ef19786fb01ebb65ea98142e9
partysoonxd <vladbelom2005@gmail.com>
2024-02-17 16:31:01 +0700

    delete auth logic

--- client/app/components/UserProvider/UserProvider.jsx
+++ client/app/components/UserProvider/UserProvider.jsx
@@ -1,52 +1,12 @@
 "use client"
 
-import { useEffect, useState } from "react";
-
 import { UserContext } from "@/app/lib/UserContext";
 import UserStore from "@/app/store/userStore";
-import { getToken } from "@/app/lib/tokenHandler";
-import { $apiHost } from "@/app/http";
 
 export default function UserProvider({children}) {
-    const userStore = new UserStore()
-    const [isAuth, setIsAuth] = useState(false)
-    
-    useEffect(() => {
-        const getCart = async(id, token) => {
-            await $apiHost.get(`/api/carts/${id}?populate=cart_items`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }).then(({data}) => {
-                userStore.setCart(data.data.attributes)
-            })
-        }
-        const authUser = async() => {
-            const token = await getToken()
-            if (token) {
-                try {
-                    await $apiHost.get('/api/users/me?fields=id&fields=email&fields=username&populate=cart', {
-                        headers: {
-                            Authorization: `Bearer ${token.value}` 
-                        }
-                    }).then(({data}) => {
-                        userStore.setUser(data)
-                        userStore.setIsAuth(true)
-                        setIsAuth(true)
-                        getCart(data.cart.id, token.value)
-                    })
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        }
-        authUser()
-        
-    }, [isAuth])
-
     return (
         <UserContext.Provider
-            value={{user: userStore}}
+            value={{user: new UserStore()}}
         >
             {children}
         </UserContext.Provider>
