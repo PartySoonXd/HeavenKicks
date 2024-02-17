db51176a00cd148070901e8ec85f8ea774b9dae1
partysoonxd <vladbelom2005@gmail.com>
2024-02-17 15:07:04 +0700

    add checkout api

--- client/app/(pages)/cart/page.jsx
+++ client/app/(pages)/cart/page.jsx
@@ -1,14 +1,37 @@
 "use client"
 
+import { loadStripe } from "@stripe/stripe-js"
+
 import { useUserContext } from "@/app/lib/UserContext"
 import { observer } from "mobx-react-lite"
+import { $apiHost } from "@/app/http"
+import { getToken } from "@/app/lib/tokenHandler"
 
 export default observer(function Cart() {
     const {user} = useUserContext()
+    const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
     let total = 0
-    // const checkoutHandler = async() => {
-        
-    // }
+    const checkoutHandler = async() => {
+        try {
+            const stripe = await stripePromise
+            const token = await getToken()
+            const cartId = user.user.cart.id
+
+            const res = await $apiHost.post("/api/orders", {
+                cartId
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token.value}`
+                }
+            })
+
+            await stripe.redirectToCheckout({
+                sessionId: res.data.stripeSession.id
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
     return (
         <main className="content">
@@ -19,7 +42,7 @@ export default observer(function Cart() {
                 })}
                 <p>{total}</p>
             </div>
-            {/* <button type="button" onClick={checkoutHandler}>CHECKOUT</button> */}
+            <button type="button" onClick={checkoutHandler}>CHECKOUT</button>
         </main>
     )
 })
