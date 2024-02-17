603dd5c8b11b735c3d791a5b0530e069112a66ef
partysoonxd <vladbelom2005@gmail.com>
2024-02-17 18:04:10 +0700

    complete footer

--- client/app/components/Newsletter/Newsletter.jsx
+++ client/app/components/Newsletter/Newsletter.jsx
@@ -1,11 +1,11 @@
 export default function Newsletter() {
     return (
         <div className="newsletter">
-            <div className="newletter-container">
-                <h1>Newsletter</h1>
+            <div className="newsletter-container">
+                <h3 className="h3 newsletter-title">Get info about <span>new arrivals</span> and <span>discounts</span></h3>
                 <form>
-                    <input type="email" placeholder="email"/>
-                    <button type="submit">SUBSCRIBE</button>
+                    <input type="email" placeholder="email" className="newsletter-input p"/>
+                    <button type="submit" className="newsletter-btn links">SUBSCRIBE</button>
                 </form>
             </div>
         </div>
