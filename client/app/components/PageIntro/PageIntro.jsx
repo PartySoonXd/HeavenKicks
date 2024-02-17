cfebb4293f2393533629cece8b88e3ac6cbeb8ad
partysoonxd <vladbelom2005@gmail.com>
2024-02-17 22:25:10 +0700

    add button

--- client/app/components/PageIntro/PageIntro.jsx
+++ client/app/components/PageIntro/PageIntro.jsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import PrettyButton from "../PrettyButton/PrettyButton";
 
 export default function PageIntro({title, image, className=""}) {
     return (
         <div className={`page-intro${className}`}>
             <h1 className="h1">{title}</h1>
             <Image src={image} width={530} height={300} alt={`${title} intro`}/>
+            {className && 
+            <div className="pretty-button-container">
+                <PrettyButton url="/catalog" text="Buy now"/>
+            </div>}
         </div>
     )
 }
