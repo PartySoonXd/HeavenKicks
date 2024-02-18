e2fc37b8429463cd4e379354c36611cd44cbc8a7
partysoonxd <vladbelom2005@gmail.com>
2024-02-18 14:07:06 +0700

    create product card component

--- client/app/components/ProductCard/ProductCard.jsx
+++ client/app/components/ProductCard/ProductCard.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image"
+
 import { $apiHost } from "@/app/http"
 import { useUserContext } from "@/app/lib/UserContext"
 import { getToken } from "@/app/lib/tokenHandler"
 
-export default function ProductCard({title, price, id}) {
+export default function ProductCard({title, price, id, image}) {
     const {user} = useUserContext()
     const getProductById = async() => {
         const {data} = await $apiHost.get(`/api/products/${id}`)
@@ -47,9 +49,14 @@ export default function ProductCard({title, price, id}) {
     }
     return (
         <li className="product-card">
-            <h3>{title}</h3>
-            <p>{price}</p>
-            <button type="button" onClick={addToCartHandler}>Add to cart</button>
+            <img src={"http://127.0.0.1:1337" + image} alt={title} className="product-card-img"/>
+            <h3 className="product-card-title h4">{title}</h3>
+            <div className="product-card-footer">
+                <p className="product-card-price h3">{price}<span>$</span></p>
+                <button type="button" onClick={addToCartHandler}>
+                    <Image src="/add-to-cart-icon.svg" width={40} height={40} alt="add to cart"/>
+                </button> 
+            </div>
         </li>
     )
 }
