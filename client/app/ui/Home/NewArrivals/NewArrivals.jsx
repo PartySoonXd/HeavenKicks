ccdd5a499f1a3c412ef7e3d9de11983e50242a36
partysoonxd <vladbelom2005@gmail.com>
2024-02-21 17:48:54 +0700

    fix new arrivals cards

--- client/app/ui/Home/NewArrivals/NewArrivals.jsx
+++ client/app/ui/Home/NewArrivals/NewArrivals.jsx
@@ -4,7 +4,7 @@ import PrettyButton from "@/app/components/PrettyButton/PrettyButton"
 export default function NewArrivals() {
     return (
         <div className="new-arrivals">
-            <h2 className="h2">NEW ARRIVALS</h2>
+            <h2 className="h2 new-arrivals-title">NEW ARRIVALS</h2>
             <Products/>
             <div className="new-arrivals-pretty-button-container">
                 <PrettyButton url="/new-arrivals" text="see all"/>
