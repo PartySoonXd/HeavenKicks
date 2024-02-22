8c3d81668859320c4f5729f57d3609cdaedae145
partysoonxd <vladbelom2005@gmail.com>
2024-02-23 01:15:13 +0700

    change route

--- client/app/(pages)/payment/success/page.jsx
+++ client/app/(pages)/payment/success/page.jsx
@@ -1,10 +1,15 @@
+"use client"
+
+import { useUserContext } from "@/app/lib/UserContext";
 import PaymentTemplate from "@/app/ui/PaymentTemplate/PaymentTemplate";
 import Link from "next/link";
 
 export default function PaymentSuccess() {
+    const {user} = useUserContext()
+
     return (
         <PaymentTemplate status="Success!" component={
-            <div className="h1">You can check your order in <a href="/contacts">profile</a></div>
+            <div className="h1">You can check your order in <Link href={`/profile/${user.user.uuid}`}>profile</Link></div>
         }/>
     )
 }
