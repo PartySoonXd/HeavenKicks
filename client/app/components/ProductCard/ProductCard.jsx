a721f00d8c19dcc76d2a12179a1adda34e7d85d6
partysoonxd <vladbelom2005@gmail.com>
2024-02-22 23:20:29 +0700

    add notification styles

--- client/app/components/ProductCard/ProductCard.jsx
+++ client/app/components/ProductCard/ProductCard.jsx
@@ -1,54 +1,53 @@
 import Image from "next/image"
-
-import { $apiHost } from "@/app/http"
-import { useUserContext } from "@/app/lib/UserContext"
-import { getToken } from "@/app/lib/tokenHandler"
 import Link from "next/link"
+
+import { useUserContext } from "@/app/lib/UserContext"
+import { addToCartHandler } from "@/app/lib/addToCartHandler"
 
 export default function ProductCard({title, price, image, slug}) {
     const {user} = useUserContext()
-    const getProductBySlug = async() => {
-        const {data} = await $apiHost.get(`/api/products?filters[slug][$eq]=${slug}&populate=images`)
-        console.log(data.data[0].attributes.images.data[0].attributes.formats.medium.url)
-        return data.data[0]
-    }
-    const updateCartStore = async(id, token) => {
-        await $apiHost.get(`/api/carts/${id}?populate=cart_items`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).then(({data}) => {
-            user.setCart(data.data.attributes)
-        })
+    // const getProductBySlug = async() => {
+    //     const {data} = await $apiHost.get(`/api/products?filters[slug][$eq]=${slug}&populate=images`)
+    //     console.log(data.data[0].attributes.images.data[0].attributes.formats.medium.url)
+    //     return data.data[0]
+    // }
+    // const updateCartStore = async(id, token) => {
+    //     await $apiHost.get(`/api/carts/${id}?populate=cart_items`, {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`
+    //         }
+    //     }).then(({data}) => {
+    //         user.setCart(data.data.attributes)
+    //     })
 
-    }
-    const addToCartHandler = async() => {
-        if (!user.isAuth) {
-            console.log('Please login to add to cart')
-            return
-        }
-        const {attributes} = await getProductBySlug()
+    // }
+    // const addToCartHandler = async() => {
+    //     if (!user.isAuth) {
+    //         console.log('Please login to add to cart')
+    //         return
+    //     }
+    //     const {attributes} = await getProductBySlug()
 
-        const cartId = user.user.cart.id
-        const token = await getToken()
-        await $apiHost.post("/api/cart-items", {
-            data: {
-                cart: {
-                    connect: [cartId]
-                },
-                title: attributes.title,
-                price: attributes.price,
-                imageURL: attributes.images.data[0].attributes.formats.medium.url,
-                size: undefined
-            },
-        }, {
-            headers: {
-                Authorization: `Bearer ${token.value}`
-            }
-        }).then(data => {
-            updateCartStore(user.user.cart.id, token.value)
-        })
-    }
+    //     const cartId = user.user.cart.id
+    //     const token = await getToken()
+    //     await $apiHost.post("/api/cart-items", {
+    //         data: {
+    //             cart: {
+    //                 connect: [cartId]
+    //             },
+    //             title: attributes.title,
+    //             price: attributes.price,
+    //             imageURL: attributes.images.data[0].attributes.formats.medium.url,
+    //             size: undefined
+    //         },
+    //     }, {
+    //         headers: {
+    //             Authorization: `Bearer ${token.value}`
+    //         }
+    //     }).then(data => {
+    //         updateCartStore(user.user.cart.id, token.value)
+    //     })
+    // }
     return (
         <li className="product-card">
             <Link href={`/product/${slug}`}>
@@ -57,7 +56,7 @@ export default function ProductCard({title, price, image, slug}) {
             </Link>
             <div className="product-card-footer">
                 <p className="product-card-price h3">{price}<span>$</span></p>
-                <button type="button" onClick={addToCartHandler}>
+                <button type="button" onClick={() => addToCartHandler(user, slug, 37)}>
                     <Image src="/add-to-cart-icon.svg" width={40} height={40} alt="add to cart"/>
                 </button> 
             </div>
