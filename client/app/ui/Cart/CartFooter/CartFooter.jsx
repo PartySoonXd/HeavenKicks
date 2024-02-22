4efe35fd286118f3fd08e512548b7599af475226
partysoonxd <vladbelom2005@gmail.com>
2024-02-23 01:14:07 +0700

    update payment

--- client/app/ui/Cart/CartFooter/CartFooter.jsx
+++ client/app/ui/Cart/CartFooter/CartFooter.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
+import { loadStripe } from "@stripe/stripe-js"
 
 import { useUserContext } from "@/app/lib/UserContext"
+import { getToken } from "@/app/lib/tokenHandler"
+import { $apiHost } from "@/app/http"
 
 export default function CartFooter () {
     const {user} = useUserContext()
@@ -11,36 +14,37 @@ export default function CartFooter () {
         })
         return total
     }
-    // const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
+    const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
 
-    // const checkoutHandler = async() => {
-    //     try {
-    //         const stripe = await stripePromise
-    //         const token = await getToken()
-    //         const cartId = user.user.cart.id
+    const checkoutHandler = async() => {
+        try {
+            const stripe = await stripePromise
+            const token = await getToken()
+            const cartId = user.user.cart.id
+            const userId = user.user.id
 
-    //         const res = await $apiHost.post("/api/orders", {
-    //             cartId
-    //         }, {
-    //             headers: {
-    //                 Authorization: `Bearer ${token.value}`
-    //             }
-    //         })
+            const res = await $apiHost.post("/api/orders", {
+                cartId, userId,
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token.value}`
+                }
+            })
 
-    //         await stripe.redirectToCheckout({
-    //             sessionId: res.data.stripeSession.id
-    //         })
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
+            await stripe.redirectToCheckout({
+                sessionId: res.data.stripeSession.id
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className="cart-footer">
             
             <h2 className="h2 cart-footer-total">Total: <span>{getTotalPrice()}$</span></h2>
             <div className="cart-footer-buttons">
                 <Link href="/catalog" className="catalog h3">CONTINUE SHOPPING</Link>
-                <button type="button" onClick={() => {}} className="checkout h3">PROCEED TO CHECKOUT</button>
+                <button type="button" disabled={user.cart?.cart_items.data.length === 0} onClick={checkoutHandler} className="checkout h3">PROCEED TO CHECKOUT</button>
             </div>
         </div>
     )
