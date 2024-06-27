a49808bf9f799be0745f7942ba9dade354a73d97
partysoonxd <vladbelom2005@gmail.com>
2024-06-28 00:41:48 +0700

    add suspense component

--- client/app/(pages)/payment/success/page.jsx
+++ client/app/(pages)/payment/success/page.jsx
@@ -6,7 +6,7 @@ import { useUserContext } from "@/app/lib/UserContext";
 import PaymentTemplate from "@/app/ui/PaymentTemplate/PaymentTemplate";
 import { observer } from "mobx-react-lite";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { $apiHost } from "@/app/http";
 import { getToken } from "@/app/lib/tokenHandler";
 
@@ -49,8 +49,10 @@ export default observer(function PaymentSuccess() {
     }, [])
 
     return (
-        <PaymentTemplate status="Success!" component={
-            <div className="h1">You can check your order in <Link href={`/profile/${user.user.uuid}`}>profile</Link></div>
-        }/>
+        <Suspense>
+            <PaymentTemplate status="Success!" component={
+                <div className="h1">You can check your order in <Link href={`/profile`}>profile</Link></div>
+            }/>
+        </Suspense>
     )
 })
