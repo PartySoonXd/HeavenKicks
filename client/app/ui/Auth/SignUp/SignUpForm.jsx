0706ec7f9abc69ee2c39e09410c3101d75bfd0dd
partysoonxd <vladbelom2005@gmail.com>
2024-06-28 00:51:12 +0700

    added notification for auth errors, password validation and show/hide password button

--- client/app/ui/Auth/SignUp/SignUpForm.jsx
+++ client/app/ui/Auth/SignUp/SignUpForm.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import Image from "next/image"
+import { useState } from "react"
 
 import { $apiHost } from "@/app/http"
 import { useUserContext } from "@/app/lib/UserContext"
@@ -10,17 +11,48 @@ import navigate from "@/app/lib/navigate"
 import InputField from "@/app/components/Auth/InputField/InputField"
 import GoogleButton from "@/app/components/Auth/GoogleButton/GoogleButton"
 import generateUUID from "@/app/lib/generateUUID"
+import Notification from "@/app/components/Notification/Notification"
 
 export default function SignUpForm() {
     const {user} = useUserContext()
 
+    const [isActive, setIsActive] = useState(false)
+    const [title, setTitle] = useState("")
+    const [text, setText] = useState("")
+
+    const [isShownPass, setIsShownPass] = useState(false)
+
+    if (isActive) {
+        setTimeout(() => {
+            setIsActive(false)
+        }, 2100)
+    }
+    const passwordValidation = (password, confirmedPassword) => {
+        const regExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+        if (password != confirmedPassword) {
+            setText("Passwords not equal.")
+            return false
+        }
+        if (password.length > 20 || password.length < 8) {
+            setText("Password must contain at least 8 and less than 20 characters long.")
+            return false
+        }
+        if (regExp.test(password)) {
+            return true 
+        } else {
+            setText("Password must contain a-z, A-Z, 0-9.")
+            return false
+        } 
+
+    }
     
     const createCart = async(id, token) => {
-        await $apiHost.post("/api/carts", {
+        await $apiHost.post("/api/carts", 
+        {
             data: {
                 user: {
                     connect: [id]
-                },
+                }
             }
         },
         {
@@ -32,13 +64,24 @@ export default function SignUpForm() {
     }
 
     const formHandler = async (e) => {
-        try {
             e.preventDefault()
             const formData = new FormData(e.currentTarget)
             const data = Object.fromEntries(formData)
             data.uuid = generateUUID()
-            if (data.password != data.confirmedPassword) {
-                console.log('passwords not equal')
+
+            if (data.username == '' || data.email == '' || 
+                data.password == '' || data.confirmedPassword == ''
+            ) {
+                setText("All fields must be complete.")
+                setTitle("Error!")
+                setIsActive(true)
+                return
+            }
+
+            const isValid = passwordValidation(data.password, data.confirmedPassword)
+            if (!isValid) {
+                setTitle("Error!")
+                setIsActive(true)
                 return
             }
 
@@ -48,13 +91,20 @@ export default function SignUpForm() {
                 user.setIsAuth(true)
                 createCart(data.user.id, data.jwt)
                 navigate("/")
+            }).catch(error => {
+                if (error?.response?.status == 400) {
+                    setText("Email or username are already in use.")
+                } else {
+                    setText("Something wrong. Try later.")
+                }
+                setTitle("Error!")
+                setIsActive(true)
             })
-        } catch (error) {
-            console.log(error)
-        }
     }
 
     return (
+        <>
+        <Notification isActive={isActive} title={title} text={text}/>
         <div className="auth-form-container">
             <Link href="/" className="auth-form-logo">
                 <Image src="/Logo.svg" width={292} height={50} alt="HeavenKicks logo"/>
@@ -63,14 +113,32 @@ export default function SignUpForm() {
             <form onSubmit={e => formHandler(e)} className="auth-form">
                 <InputField type="text" placeholder="username" name="username"/>
                 <InputField type="email" placeholder="email" name="email"/>
-                <InputField type="password" placeholder="password" name="password"/>
-                <InputField type="password" placeholder="confirm password" name="confirmedPassword"/>
-    
+                <div className="auth-form-password-wrapper">
+                    <InputField type={isShownPass ? "text": "password"} placeholder="password" name="password"/>
+                    <Image 
+                        src={isShownPass ? "/show-pass-icon.png" : "/hide-pass-icon.png"} 
+                        width={20} 
+                        height={20}
+                        alt="shown password"
+                        onClick={() => setIsShownPass(!isShownPass)}
+                    />
+                </div>
+                <div className="auth-form-password-wrapper">
+                    <InputField type={isShownPass ? "text": "password"} placeholder="confirm password" name="confirmedPassword"/>
+                    <Image 
+                        src={isShownPass ? "/show-pass-icon.png" : "/hide-pass-icon.png"} 
+                        width={20} 
+                        height={20}
+                        alt="shown password"
+                        onClick={() => setIsShownPass(!isShownPass)}
+                    />
+                </div>
                 <button type="submit" className="auth-form-button links">SIGN UP</button>
             </form>
             <span className="h4">or</span>
             <GoogleButton/>
             <h4 className="h4 auth-form-link">Already have an account? <Link href="/sign-in">Sign in</Link></h4>
         </div>
+        </>
     )
 }
