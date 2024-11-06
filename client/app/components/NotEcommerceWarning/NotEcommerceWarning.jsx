07975a25757e2629aa45f0331da725e1554020aa
PartySoonXd <vladbelom2005@gmail.com>
2024-11-06 22:31:36 +0700

    created component with warning

---
+++ client/app/components/NotEcommerceWarning/NotEcommerceWarning.jsx
@@ -0,0 +1,7 @@
+export default function NotEcommerceWarning() {
+    return (
+        <div className="warning">
+            <h4 className="warning-text h4">This site NOT e-commerce store. He made as a portfolio project for representation of my skills.</h4>
+        </div>
+    )
+}
