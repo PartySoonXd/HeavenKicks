4c32644a95f5be8f1d255e8ec9994af88ffc3676
PartySoonXd <vladbelom2005@gmail.com>
2024-11-10 23:20:08 +0700

    added email provider plugin

--- admin/config/plugins.js
+++ admin/config/plugins.js
@@ -1,7 +1,25 @@
-module.exports = () => ({
+module.exports = ({env}) => ({
     "strapi-google-auth": {
         enabled: true,
     },
+    email: {
+        config: {
+          provider: 'nodemailer',
+          providerOptions: {
+            host: env('SMTP_HOST', 'smtp.example.com'),
+            port: env('SMTP_PORT', 587),
+            auth: {
+              user: env('SMTP_USERNAME'),
+              pass: env('SMTP_PASSWORD'),
+            },
+            // ... any custom nodemailer options
+          },
+          settings: {
+            defaultFrom: 'hello@example.com',
+            defaultReplyTo: 'hello@example.com',
+          },
+        },
+      },
     redis: {
         config: {
             connections: {
