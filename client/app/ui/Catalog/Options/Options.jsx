d3df6de7baa9f0d00e5daed5f4f8c98c03a892a5
PartySoonXd <vladbelom2005@gmail.com>
2024-12-04 23:24:04 +0700

    reworked options method

--- client/app/ui/Catalog/Options/Options.jsx
+++ client/app/ui/Catalog/Options/Options.jsx
@@ -1,16 +1,56 @@
 "use client"
 
 import Image from "next/image"
+import { useSearchParams } from "next/navigation"
 import { useState } from "react"
 
-export default function Options({setIsActive, setSort, setSearch}) {
+export default function Options({setIsActive, setFilters}) {
     const [selectIsActive, setSelectIsActive] = useState(false)
     const [selectValue, setSelectValue] = useState("sort")
 
+    const [search, setSearch] = useState("")
+    const [sort, setSort] = useState("")
+
+    const searchParams = useSearchParams()
+    const params = new URLSearchParams(searchParams)
+    
+    const timers = []
+
     const selectHandler = (value, sort) => {
+        setSort(sort)
         setSelectValue(value)
         setSelectIsActive(false)
-        setSort(sort)
+        
+        params.append("options[sort]", sort)
+        params.append("options[search]", search)
+
+        for (let i = 0; i < timers.length; i++) {
+            clearTimeout(timers[i])
+        }
+                    
+        const timer = setTimeout(() => {
+            const decodedParams = decodeURIComponent(params.toString());
+            setFilters(decodedParams)
+        }, 300)
+
+        timers.push(timer)
+    }
+    
+    const searchHandler = (search) => {
+        setSearch(search)
+        params.append("options[sort]", sort)
+        params.append("options[search]", search)
+
+        for (let i = 0; i < timers.length; i++) {
+            clearTimeout(timers[i])
+        }
+
+        const timer = setTimeout(() => {
+            const decodedParams = decodeURIComponent(params.toString());
+            setFilters(decodedParams)
+        }, 750)
+
+        timers.push(timer)
     }
     return (
         <div className="options">
@@ -27,21 +67,28 @@ export default function Options({setIsActive, setSort, setSearch}) {
                     <div className="options-select-dropdown">
                         <div 
                             className="options-select-dropdown-item p" 
-                            onClick={() => selectHandler("Price ↓", "price:desc")}
+                            onClick={() => selectHandler("Price ↓", "desc")}
                         >
                             Price ↓
                         </div>
                         <div 
                             className="options-select-dropdown-item p" 
                             name="fromLowToHigh" 
-                            onClick={() => selectHandler("Price ↑", 'price:asc')}
+                            onClick={() => selectHandler("Price ↑", "asc")}
                         >
                             Price ↑
                         </div>
                     </div>
                 }
             </div>
-            <input type="text" className="options-search p" name="search-input" placeholder="search" onChange={(e) => setSearch(e.target.value)}/>
+            <input  
+                type="text" 
+                className="options-search p" 
+                name="search-input" 
+                placeholder="search" 
+                onChange={(e) => searchHandler(e.target.value)}
+                maxLength={65}
+            />
         </div>
     )
 }
