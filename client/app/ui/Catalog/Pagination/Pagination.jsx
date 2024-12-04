cadd39387b9aa04499e61237aa83982e8dee5e10
PartySoonXd <vladbelom2005@gmail.com>
2024-12-04 23:23:32 +0700

    made pagination save after page refresh

--- client/app/ui/Catalog/Pagination/Pagination.jsx
+++ client/app/ui/Catalog/Pagination/Pagination.jsx
@@ -1,19 +1,26 @@
 "use client"
 
 import Image from "next/image"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
-export default function Pagination({pagination, setCurrentPage, currentPage, containerRef}) {
+export default function Pagination({pagination}) {
+    const router = useRouter()
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+    
     const handlePagination = (page) => {
-        setCurrentPage(page)
-        containerRef.current.scrollIntoView()
+        const params = new URLSearchParams(searchParams)
+        params.set("page", page)
+        
+        router.push(pathname + "?" + params.toString())
     }
     if (pagination.pageCount > 0) {
         return (
             <div className="pagination">
                 <button 
                     type="button" 
-                    disabled={currentPage === 1} 
-                    onClick={() => handlePagination(currentPage-1)} 
+                    disabled={searchParams.has('page') && searchParams.get('page') == 1} 
+                    onClick={() => handlePagination(searchParams.get('page') - 1)} 
                     className="minus-arrow"
                 >
                     <Image src="/arrow-icon.svg" width={35} height={15} alt="pagination button"/>
@@ -22,7 +29,7 @@ export default function Pagination({pagination, setCurrentPage, currentPage, con
                     {pagination && [...Array(pagination.pageCount)].map((item, i) => {
                         return (
                             <li 
-                                className={`pagination-item h4 ${currentPage === i+1 && "active"}`} 
+                                className={`pagination-item h4 ${searchParams.get('page') == i+1 && "active"}`} 
                                 key={i}
                                 onClick={() => handlePagination(i+1)}
                             >{i+1}</li>
@@ -31,8 +38,8 @@ export default function Pagination({pagination, setCurrentPage, currentPage, con
                 </ul>
                 <button 
                     type="button" 
-                    disabled={currentPage >= pagination.pageCount} 
-                    onClick={() => handlePagination(currentPage+1)} 
+                    disabled={searchParams.get('page') >= pagination.pageCount} 
+                    onClick={() => handlePagination(parseInt(searchParams.get('page')) + 1)} 
                     className="plus-arrow"
                 >
                     <Image src="/arrow-icon.svg" width={35} height={15} alt="pagination button"/>
